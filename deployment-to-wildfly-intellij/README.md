da93ebedbf74f9bc85f594da86eea8c7745cee76
Philip Riecks <mail@philipriecks.de>
2020-01-04 18:03:57 +0100

    add new project for deployment on WildFly with IntelliJ

---
+++ deployment-to-wildfly-intellij/README.md
@@ -0,0 +1,3 @@
+# Deploy a Jakarta EE application to WildFly using IntelliJ
+
+Source code for the video on YouTube: [Deploy and debug a Jakarta EE application with WildFly using IntelliJ IDEA]()
---
+++ deployment-to-wildfly-intellij/src/main/java/de/rieckpil/quickstarts/JAXRSConfiguration.java
@@ -0,0 +1,8 @@
+package de.rieckpil.quickstarts;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+}
