00f8b03c9c60ab7822a893b507e108fd049bc076
Philip Riecks <mail@philipriecks.de>
2020-01-12 10:39:52 +0100

    update MicroShed example

--- integration-tests-with-microshed-testing/src/test/java/de/rieckpil/blog/SampleResourceIT.java
+++ integration-tests-with-microshed-testing/src/test/java/de/rieckpil/blog/SampleResourceIT.java
@@ -1,15 +1,21 @@
 package de.rieckpil.blog;
 
 import org.junit.jupiter.api.Test;
-import org.microshed.testing.SharedContainerConfig;
 import org.microshed.testing.jaxrs.RESTClient;
 import org.microshed.testing.jupiter.MicroShedTest;
+import org.microshed.testing.testcontainers.ApplicationContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicroShedTest
-@SharedContainerConfig(SampleApplicationConfig.class)
 public class SampleResourceIT {
+
+  @Container
+  public static ApplicationContainer app = new ApplicationContainer()
+    .withEnv("message", "Hello World from MicroShed Testing")
+    .withAppContextRoot("/")
+    .withReadinessPath("/health/ready");
 
   @RESTClient
   public static SampleResource sampleEndpoint;
